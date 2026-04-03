5241ef311e015cff4759a379085c8cc8913e621a
twosatsmaxi <twosatsmaxi@gmail.com>
2026-04-03 09:54:03 +0530

    Add /gallery API endpoint (#4508)

--- src/api.rs
+++ src/api.rs
@@ -90,6 +90,13 @@ pub struct ChildInscriptions {
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Gallery {
+  pub ids: Vec<InscriptionId>,
+  pub more: bool,
+  pub page: usize,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct ParentInscriptions {
   pub parents: Vec<RelativeInscriptionRecursive>,
   pub more: bool,
